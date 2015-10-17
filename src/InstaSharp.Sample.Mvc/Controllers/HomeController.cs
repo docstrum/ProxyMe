b08f9ce024e16245641322c47139a59f13ddc878
docstrum <fmr@msn.com>
2015-10-17 10:11:35 -0400

    full

--- src/InstaSharp.Sample.Mvc/Controllers/HomeController.cs
+++ src/InstaSharp.Sample.Mvc/Controllers/HomeController.cs
@@ -11,16 +11,14 @@ namespace InstaSharp.Sample.Mvc.Controllers
     {
         static string clientId = "f14134ed24754b658b616e1ce855d350";
         static string clientSecret = "1262e676faeb4eb0a3b42928c4fc3147";
-        static string redirectUri = "http://localhost:5969/Home/OAuth";
-        //static string redirectUri = "http://www.proxme.net/Home/OAuth";
+        //static string redirectUri = "http://localhost:5969/Home/OAuth";
+        static string redirectUri = "http://www.proxme.net/Home/OAuth";
 
         InstagramConfig config = new InstagramConfig(clientId, clientSecret, redirectUri, "");
 
-        // GET: Home
         public ActionResult Index()
         {
             var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
-
             if (oAuthResponse == null)
             {
                 return RedirectToAction("Login");
@@ -29,7 +27,6 @@ namespace InstaSharp.Sample.Mvc.Controllers
             {
                 return RedirectToAction("NearMe");
             }
-
             return View(oAuthResponse.User);
         }
 
@@ -38,41 +35,31 @@ namespace InstaSharp.Sample.Mvc.Controllers
             var scopes = new List<OAuth.Scope>();
             scopes.Add(InstaSharp.OAuth.Scope.Likes);
             scopes.Add(InstaSharp.OAuth.Scope.Comments);
-
             var link = InstaSharp.OAuth.AuthLink(config.OAuthUri + "authorize", config.ClientId, config.RedirectUri, scopes, InstaSharp.OAuth.ResponseType.Code);
-
             return Redirect(link);
         }
 
         public async Task<ActionResult> MyFeed()
         {
             var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
-
             if (oAuthResponse == null)
             {
                 return RedirectToAction("Login");
             }
-
             var users = new Endpoints.Users(config, oAuthResponse);
-
             var feed = await users.Feed(null, null, null);
-
             return View(feed.Data);
         }
 
         public async Task<ActionResult> MyPosts()
         {
             var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
-
             if (oAuthResponse == null)
             {
                 return RedirectToAction("Login");
             }
-
             var users = new Endpoints.Users(config, oAuthResponse);
-
             var feed = await users.RecentSelf();
-
             return View(feed.Data);
         }
 
@@ -84,51 +71,37 @@ namespace InstaSharp.Sample.Mvc.Controllers
             {
                 return RedirectToAction("Login");
             }
-
             var locations = new Endpoints.Media(config, oAuthResponse);
-
             var geo = new Endpoints.Geographies(config, oAuthResponse);
-
             var start = System.DateTime.Now - System.TimeSpan.FromDays(1);
-
             var end = System.DateTime.Now;
-
             //var locFeed = await locations.Search(49, -81, 5000, start, end); //41.285765, -81.8548987
-
             var posts = new List<WallElement>();
-
             return View(posts);
         }
 
         [HttpPost]
         public async Task<ActionResult> NearMe2(double latitude, double longitude)
         {
+            if (latitude == 0)
+            {
+                return View();
+            }
             ModelState.Clear();
-
             var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
-
             if (oAuthResponse == null)
             {
                 return RedirectToAction("Login");
             }
-
             var locations = new Endpoints.Media(config, oAuthResponse);
-
             var geo = new Endpoints.Geographies(config, oAuthResponse);
-
             var start = System.DateTime.Now - System.TimeSpan.FromDays(1);
-
             var end = System.DateTime.Now;
-
             var locFeed = await locations.Search(latitude, longitude, 5000, start, end);
-
             var posts = new List<WallElement>();
-
             foreach (var post in locFeed.Data)
             {
                 var distance = CalculateDistance(latitude, post.Location.Latitude, longitude, post.Location.Longitude);
-
-
                 posts.Add(new WallElement()
                 {
                     CreatedTime = post.CreatedTime.ToLocalTime(),
@@ -144,34 +117,24 @@ namespace InstaSharp.Sample.Mvc.Controllers
                     LowResoltionUrl = post.Images.LowResolution.Url,
                     ThumbnailUrl = post.Images.Thumbnail.Url,
                     Username = post.User.Username
-
                 });
             }
-
             posts = posts.OrderBy(x => x.Distance).ToList();
-
             return View(posts);
         }
 
         public async Task<ActionResult> WhoIsNear()
         {
             var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
-
             if (oAuthResponse == null)
             {
                 return RedirectToAction("Login");
             }
-
             var locations = new Endpoints.Media(config, oAuthResponse);
-
             var geo = new Endpoints.Geographies(config, oAuthResponse);
-
             var start = System.DateTime.Now - System.TimeSpan.FromDays(1);
-
             var end = System.DateTime.Now;
-
             var locFeed = await locations.Search(49, -81, 5000, start, end); //41.285765, -81.8548987
-
             //var goFeed = await geo.Recent()
             ModelState.Clear();
             return View(locFeed.Data);
@@ -181,48 +144,26 @@ namespace InstaSharp.Sample.Mvc.Controllers
         public async Task<ActionResult> WhoIsNear2(double latitude, double longitude)
         {
             ModelState.Clear();
-
             var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
-
             if (oAuthResponse == null)
             {
                 return RedirectToAction("Login");
             }
-
             var locations = new Endpoints.Media(config, oAuthResponse);
-
             var geo = new Endpoints.Geographies(config, oAuthResponse);
-
             var start = System.DateTime.Now - System.TimeSpan.FromDays(1);
-
             var end = System.DateTime.Now;
-
             var locFeed = await locations.Search(latitude, longitude, 5000, start, end);
-
-
-            //var goFeed = await geo.Recent()
-
             ViewBag.latitude = System.Convert.ToDecimal(latitude);
             ViewBag.longitude = System.Convert.ToDecimal(longitude);
-
             return View(locFeed.Data);
         }
 
         public async Task<ActionResult> OAuth(string code)
         {
-            // add this code to the auth object
             var auth = new OAuth(config);
-
-            // now we have to call back to instagram and include the code they gave us
-            // along with our client secret
             var oauthResponse = await auth.RequestToken(code);
-
-            // both the client secret and the token are considered sensitive data, so we won't be
-            // sending them back to the browser. we'll only store them temporarily.  If a user's session times
-            // out, they will have to click on the authenticate button again - sorry bout yer luck.
             Session.Add("InstaSharp.AuthInfo", oauthResponse);
-
-            // all done, lets redirect to the home controller which will send some initial data to the app
             return RedirectToAction("Index");
         }
 
@@ -236,7 +177,7 @@ namespace InstaSharp.Sample.Mvc.Controllers
             var dlon = lon2 - lon1;
             var a = System.Math.Pow(System.Math.Sin(dlat / 2), 2) + System.Math.Cos(lat1) * System.Math.Cos(lat2) * System.Math.Pow(System.Math.Sin(dlon / 2), 2);
             var c = 2 * System.Math.Atan2(System.Math.Sqrt(a), System.Math.Sqrt(1 - a));
-            var d = 3961 * c; //using miles 6373 using km
+            var d = 3961 * c; //use 6373 for km
             var distance = System.Math.Round(d * 100) / 100;
             return distance; 
         }
